6c7694b22d62806f8a04be401065b3ba769b2e45
ines <ines@192.168.10.173>
2015-10-28 15:50:37 +0100

    adding method
    Signed-off-by: ines <ines@192.168.10.173>

--- calculator/src/main/java/edu/esprit/calculator/Calculator.java
+++ calculator/src/main/java/edu/esprit/calculator/Calculator.java
@@ -11,4 +11,20 @@ public class Calculator {
 		}
 		return result;
 	}
+
+	public Long multiply(Long a,Long b)
+	{
+		return a*b;
+	}
+
+	
+	public Double divide (Double d1, Double d2)
+	{
+		if (d2 == 0)
+			throw new IllegalArgumentException("can't divide by 0!!");
+	
+		return d1/d2;
+	}
+	
+
 }
